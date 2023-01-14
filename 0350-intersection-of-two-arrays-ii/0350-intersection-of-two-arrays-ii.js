0f86efbe1879aca50518ecd52b4fb9559807318c
Reginald Dunlap <71197754+Flatout007@users.noreply.github.com>
2023-01-14 12:40:04 -0500

    Time: 70 ms (80.16%), Space: 43.8 MB (47.66%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    
+    const p = new Program(nums1, nums2, [], {});
+    
+    return p.findIntersection();
+};
+
+class Program {
+    
+    constructor(nums1, nums2, intersectionArray, hashtable) {
+        this.nums1 = nums1;
+        this.nums2 = nums2.sort((a,b) => a-b);
+        this.intersectionArray = intersectionArray;
+        this.hashtable = hashtable;
+    }
+
+    getFrequencies(arr) {
+        for (let i = 0; i<arr.length; i++) {
+            
+            const num = arr[i];
+            
+            if (this.hashtable[num] != null) {
+                this.hashtable[num] += 1;
+            } else {
+                this.hashtable[num] = 1;
+            }
+        }
+    }
+    
+    findIntersection() {
+        
+        this.getFrequencies(this.nums2);
+        
+        for (let i = 0; i<this.nums1.length; i++) {
+            
+            const num = this.nums1[i];
+            const frequency = this.hashtable[num];
+            
+            if (frequency != null) {
+                
+                this.intersectionArray.push(num);
+                
+                this.hashtable[num] -= 1;
+                
+                if (this.hashtable[num] === 0)
+                delete this.hashtable[num];
+                    
+            }
+        }
+        
+        return this.intersectionArray;
+    }
+}
