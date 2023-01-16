757620016143422061db8dfb12b86bb74b930ba9
Reginald Dunlap <71197754+Flatout007@users.noreply.github.com>
2023-01-16 11:53:24 -0500

    Time: 95 ms (76.66%), Space: 43.2 MB (94.05%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -8,7 +8,7 @@ var canConstruct = function(ransomNote, magazine) {
     
     // use a hashtable to get frequencies of a.
     // loop through b and subtract relative frequencies. 
-    // if a's frequecnies are all 0 after looping,
+    // if a's frequecnies are all 0 or less after looping,
     // return true, else false
     
     const p = new Program(ransomNote, magazine, {});
