e7f0336c2c63710925126c28d8cf2bf6b612dd17
Reginald Dunlap <71197754+Flatout007@users.noreply.github.com>
2023-01-17 20:27:11 -0500

    Time: 73 ms (78.61%), Space: 44.3 MB (36.39%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -13,7 +13,7 @@
 var mergeTwoLists = function(list1, list2) {
     
     // use two pointers and tarverse both lists simultaneously and use the merge 
-    // techique to merge lists a and b 
+    // technique to merge lists a and b 
     
     const newList = new ListNode();
     const p = new Program(list1, list2, newList);
@@ -53,12 +53,10 @@ class Program {
                 this.newList.val = valueB;
                 b = nextB;
             }
-            
        }
         
         while (a != null) {
             
-            const node = a;
             const next = new ListNode();
             
             this.newList.next = next;
@@ -71,7 +69,6 @@ class Program {
         
         while (b != null) {
             
-            const node = b;
             const next = new ListNode();
             
             this.newList.next = next;
