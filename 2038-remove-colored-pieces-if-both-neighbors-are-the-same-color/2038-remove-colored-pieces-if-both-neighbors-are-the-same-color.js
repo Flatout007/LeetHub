22fa3d15697c47297d31704dd129133be36482ef
Reginald Dunlap <71197754+Flatout007@users.noreply.github.com>
2023-01-19 15:29:39 -0500

    Time: 120 ms (49.48%), Space: 45.9 MB (58.76%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
@@ -4,12 +4,6 @@
  */
 var winnerOfGame = function(colors) {
 
-    // brute force:
-    // use alternating loops to loop through colors and find 
-    // 3 matching adjacent colors, remove middle color and continue to loop until 
-    // a player can't find 3 adjacent colors
-    
-    // efficient: 
     // Count the contigious occurences of A and B
     
     const players = {"alice": 0, "bob": 0};
@@ -48,61 +42,3 @@ class Program {
     }
 }
 
-// class Program {
-    
-//     constructor(colors) {
-//         this.colors = colors;
-//     }
-    
-//     deleteFromString(str, index) {
-        
-//         let newString = "";
-        
-//         for (let i = 0; i<str.length; i++) {
-//             if (i !== index)
-//             newString += str[i];
-//         }
-        
-//         return newString;
-//     }
-    
-//     winner() {
-        
-//        let i = 0; 
-        
-//        while (true) {
-           
-//            if (i % 2 === 0) {
-               
-//                for (let i = 0; i<this.colors.length; i++) {
-                   
-//                   if (this.colors[i+2] == null)
-//                   return false;
-                   
-//                    if (this.colors[i] === "A" && this.colors[i+1] === "A" && this.colors[i+2] === "A") {
-//                        this.colors = this.deleteFromString(this.colors, i+1);
-//                        break;
-//                    } 
-                   
-//                }
-               
-//            } else {
-               
-//                for (let i = 0; i<this.colors.length; i++) {
-                   
-//                   if (this.colors[i+2] == null)
-//                   return true;
-                   
-//                    if (this.colors[i] === "B" && this.colors[i+1] === "B" && this.colors[i+2] === "B") {
-//                        this.colors = this.deleteFromString(this.colors, i+1);
-//                        break;
-//                    } 
-                   
-                  
-//                }
-//            }
-           
-//            i++;
-//        }
-//     }
-// }
