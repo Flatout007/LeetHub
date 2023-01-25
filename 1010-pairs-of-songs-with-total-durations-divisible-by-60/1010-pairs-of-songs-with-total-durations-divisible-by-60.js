997f65e5816fe8685d8393d4a2a4c957ba1b79e1
Reginald Dunlap <71197754+Flatout007@users.noreply.github.com>
2023-01-25 13:48:03 -0500

    Time: 8314 ms (8.37%), Space: 47.7 MB (8.81%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.js
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.js
@@ -4,22 +4,6 @@
  */
 var numPairsDivisibleBy60 = function(time) {
     
-    
-    // brute force:
-    // sum every possible pair checking if its divisible by 60
-    
-    // binary search: 
-    // for each time[i], binary search for the target.
-    // The target being any number that sums with time[i] to become divisible by 60. 
-    // after binary search, the number in left or right halves must add up to a multiple of 60 with time[i].
-    // use a loop to search for that number between left and right, if found, increment the count.
-    
-    
-    // two pointer:
-    // sort array, use two pointers to check if their sum is divisible by 60 then
-    // increment or decrement the correct pointer, continue this loop
-    // until left is greater than right
-    
     time.sort((a,b) => a-b);
     
     const p = new Program(time, 0);
@@ -37,32 +21,12 @@ class Program {
         this.count = count;
     }
     
-//     search(left, right, number) {
-//         while (left <= right) {
-                
-//                 let sum = 0;
-//                 const mid = Math.floor((left + right) / 2);
-                
-//                 sum = number + this.time[mid];
-                
-//                 if (sum % 60 === 0) {
-//                     return mid;
-//                 } else if (this.time[mid] < 60 || number < 60) {
-//                     left = mid + 1;
-//                 } else {
-//                     right = mid - 1;
-//                 }
-//             }
-            
-//             return -1;
-//     }
-    
     numPairs() {
         
         for (let i = 0; i<this.time.length; i++) {
             
             const number = this.time[i];
-            let left = i + 1; // left is i+1 to avoid using same element
+            let left = i + 1; // left is i+1 keep left in front of times[i], avoiding same element
             let right = this.time.length-1;
             
             while (left <= right) {
@@ -73,12 +37,11 @@ class Program {
                 sum = number + this.time[mid];
                 
                 if (sum < 60) {
-                    left = mid + 1;
+                    left = mid + 1; // narrow search to numbers greater than 60!
                 } else {
-                    right = mid - 1;
+                    right = mid - 1; // narrow search to numbers less than or equal to 60!
                 }
             }
-            
     
            //  The array is now divided in half.
            //  The number in left or right halves must add up to a multiple of 60 with time[i].
