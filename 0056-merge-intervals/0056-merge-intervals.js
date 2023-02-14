eebf98722491c6f5e03732f59e724f85ec71e3a0
Reginald Dunlap <71197754+Flatout007@users.noreply.github.com>
2023-02-14 17:11:08 -0500

    Time: 106 ms (60.87%), Space: 48.6 MB (62.81%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    
+    /**@Summary:
+
+    Two sets of time intervals A and B are "mergeable" if and only if there is any intersection 
+    between the intervals in set A and the intervals in set B.
+    */
+    
+    const newIntervals = [];
+    let left = 0;
+    let right = 1;
+    
+    intervals.sort((a,b) => a[0] - b[0]);
+    
+    for (let i = 0; i<intervals.length; i++, left++, right++) {
+        
+        if (right >= intervals.length) {
+            break;
+        }
+            
+        const startA = intervals[left][0];
+        const endA = intervals[left][1];
+        const startB = intervals[right][0];
+        const endB = intervals[right][1];
+        
+        // if sets of times interscect, merge sets of time.
+        if (startB <= endA && startB >= startA) {
+            
+            const min = Math.min(startA, startB);
+            const max = Math.max(endA, endB);
+           
+            intervals[i] = null;
+            intervals[i+1] = [min, max]; 
+        } 
+    }
+    
+    return intervals.filter(ele => ele != null);
+}
